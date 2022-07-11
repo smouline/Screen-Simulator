c187c3c118d5f2d75aef661b41c8ae0a929c206d
smouline <108255302+smouline@users.noreply.github.com>
2022-07-11 14:50:45 -0700

    12 adding library labels reorganizing methods documentation (#29)
    * added labels to control and treatment libraries, edited documentation, reorganized methods so that the sample method is clearer
    
    * rewriting control and treatment unit tests based on reorganizing done in simulator.py

--- test/test_simulator.py
+++ test/test_simulator.py
@@ -1,7 +1,7 @@
 import pytest
 from screenSim.simulator import Simulator
 
-# some tests, will add more 
+# add more later  
 
 trial = Simulator(num_genes = 10)
 
@@ -19,20 +19,20 @@ def test_totals_array_2():
     assert len(trial.totals_array) == trial.num_control + trial.num_treatment
 
 def test_num_types():
-    assert trial.num_e + trial.num_d + trial.num_ntc + trial.num_n == trial.num_genes
-
+    assert len(trial.g_e) + len(trial.g_d) + len(trial.g_ntc) + len(trial.g_n) == trial.num_genes
+    
 def test_lambda(): 
     for i in trial.lam:
         assert (i > trial.bounds[0]) & (i < trial.bounds[1])
-
-def test_treatment_sums():
-    treatments = trial._setting_treatment_libraries()
-    total = trial.totals_array
-    for i in range(len(treatments)):
-        assert abs((treatments[i].sum() - total[i])/total[i]) < 0.05
 
 def test_control_sums():
     controls = trial._setting_control_libraries()
     total = trial.totals_array
     for i in range(len(controls)):
-        assert abs((controls[i].sum() - total[-(i+1)])/total[-(i+1)]) < 0.05
+        assert abs((controls[i].sum() - total[i])/total[i]) < 0.05
+
+def test_treatment_sums():
+    treatments = trial._setting_treatment_libraries()
+    total = trial.totals_array
+    for i in range(len(treatments)):
+        assert abs((treatments[i].sum() - total[-(i+1)])/total[-(i+1)]) < 0.05
