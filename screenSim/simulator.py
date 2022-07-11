58a12cb2d2429837a7ea29c6443df9e4374d17e3
smouline <safaamouline@berkeley.edu>
2022-07-11 13:09:22 -0700

    added option to choose negative binomial or poisson distrubution in a way where more distributions can be added later

--- screenSim/simulator.py
+++ screenSim/simulator.py
@@ -1,53 +1,151 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 class Simulator:
     def __init__(
         self, 
         num_genes = 20000, 
-        avg_num_sgRNAs = 5, 
-        num_treatment = 2, 
+        avg_num_sgRNAs = 5,  
         num_control = 2, 
+        num_treatment = 2,
         min_total = 1000,
         max_total = 100000,
         total_NTCs = 1000,
         fraction_enriched = 0.2,
         fraction_depleted = 0.2,
-        fraction_NTC = 0.2):
+        fraction_NTC = 0.2,
+        type_dist = 2):
 
-        self.totals_array = np.random.randint(self.min_total, self.max_total, size = self.num_treatment + self.num_control)
-
+        """
+        Constructor for initializing Simulator object.
+        fraction_depleted : float
+            The fraction of depleted genes with respect to all genes. 
+        fraction_NTC : float
+            The fraction of NTC genes with respect to all genes.
+        type_dist : int
+            1 is poisson distrubution and 2 is negative binomial distrubution. 
+            
+        Raises
+        ------
+    @@ -50,40 +53,39 @@ def __init__(
+        """
+        self.num_genes = num_genes
+        self.avg_num_sgRNAs = avg_num_sgRNAs
+        self.num_control = num_control
+        self.num_treatment = num_treatment
+        self.min_total = min_total
+        self.max_total = max_total
+        self.total_NTCs = total_NTCs
+        self.type_dist = type_dist
         self.bounds = [10, 30]
 
-        self.sgRNAs = self._num_sgRNAs()
+        self._init_count_totals()
+        self._init_fractions(fraction_enriched, fraction_depleted, fraction_NTC)
+        self._init_num_sgRNAs()
+        self._split_genes()
+        self._init_lambda()
+        self._init_p()
+        self._init_S()
 
-        self.num_e = round(len(self.sgRNAs) * self.fraction_enriched)
-        self.num_d = round(len(self.sgRNAs) * self.fraction_depleted)
-        self.num_ntc = round(len(self.sgRNAs) * self.fraction_NTC)
-        self.num_n = round(len(self.sgRNAs) * self.fraction_normal)
 
-        self.lam = [np.random.uniform(self.bounds[0], self.bounds[1]) for gene in self.sgRNAs for num in np.arange(gene)]
 
-        #make option of whether nbinomial or poisson, trucated normal 
+    def _init_count_totals(self):
+        self.totals_array = np.random.randint(self.min_total, self.max_total, size = self.num_treatment + self.num_control)
+
+    def _init_fractions(self, e, d, ntc):
+        total = e + d + ntc
+
+        if ((total > 0.0) & (total <= 1.0)):
+            self.fraction_enriched = e
+            self.fraction_depleted = d
+            self.fraction_NTC = ntc
+            self.fraction_normal = 1.0 - (e + d + ntc)
+        else:
+            raise Exception("Fractions total cannot exceed 1.") 
+
+    def _init_num_sgRNAs(self):
+        """
+        Generates a number of sgRNAs per gene. 
+        
+    @@ -96,9 +98,66 @@ def _num_sgRNAs(self):
+        """
+        sgRNAs = np.random.normal(loc=self.avg_num_sgRNAs, scale=1, size=self.num_genes)
+        sgRNAs = np.round(sgRNAs)
+        self.sgRNAs = sgRNAs 
+
+    def _split_genes(self):
+        num_e = round(len(self.sgRNAs) * self.fraction_enriched)
+        num_d = round(len(self.sgRNAs) * self.fraction_depleted)
+        num_ntc = round(len(self.sgRNAs) * self.fraction_NTC)
+        num_n = round(len(self.sgRNAs) * self.fraction_normal)
+
+        self.g_e = self.sgRNAs[0: num_e]
+        self.g_d = self.sgRNAs[num_e: num_e + num_d]
+        self.g_ntc = self.sgRNAs[num_e + num_d: num_e + num_d + num_ntc]
+        self.g_n = self.sgRNAs[num_e + num_d + num_ntc: num_e + num_d + num_ntc + num_n]
+
+    def _init_lambda(self):
+        self.lam = np.random.uniform(self.bounds[0], self.bounds[1], size = int(self.sgRNAs.sum()))
+
+    def _init_p(self):
+        self.p = np.random.random(size = int(self.sgRNAs.sum()))
+
+    def _init_S(self):
+        S = []
+
+        for i in self.g_e:
+            g_scalar = np.random.uniform(1.2, 2.0)
+            for n in np.arange(i):
+                S.append(g_scalar)
+
+        for i in self.g_d:
+            g_scalar = np.random.uniform(0.2, 1.0)
+            for n in np.arange(i):
+                S.append(g_scalar)
+
+        for i in self.g_ntc:
+            for n in np.arange(i):
+                S.append(1)
+
+        for i in self.g_n:
+            for n in np.arange(i):
+                S.append(1)
+
+        self.S = S 
 
     def _sgRNAs(self):
         return ["sgRNA_" + str(int(i)) for i in np.arange(self.sgRNAs.sum())]
 
     def _gene(self):
+        """
+        Generates list of numbered genes for use in sample() DataFrame. 
         
+        Returns
+        -------
+        list
+            The elements of the list are in numerical order up to the 
+            number of genes in `num_genes`.
+        
+        """
         return ["gene_" + str(i) for i in np.arange(len(self.sgRNAs)) for n in np.arange(self.sgRNAs[i])]
 
 
-    def _num_sgRNAs(self):
-        sgRNAs = np.random.normal(loc=self.avg_num_sgRNAs, scale=1, size=self.num_genes)
-        sgRNAs = np.round(sgRNAs)
-        return sgRNAs 
+    def _sum_array(self, index, lambdas, p_array):
+        """
+        Creates an array of random integers with a specified sum.
+        
+    @@ -114,10 +173,20 @@ def _sum_array(self, index):
+            array of randomly generated integers with sum of element from `totals_array`    
+        
+        """
 
+        if self.type_dist == 1:
+            a = [np.random.poisson(i, size=1) for i in lambdas]
+        elif self.type_dist == 2:
+            a = [np.random.negative_binomial(i, p, size=1) for i in lambdas for p in p_array]
+        else:
+            raise Exception("Make sure to choose a type from the available ints")
 
-    def _sum_array(self, index, lambdas):
-
-        a = [np.random.poisson(i, size=1) for i in lambdas]
         a = np.concatenate(a)
         a = a.astype(float)
         a /= (a.sum())
@@ -60,7 +158,7 @@ class Simulator:
         treatment = [] 
 
         for i in np.arange(self.num_treatment):
-            treatment.append(self._sum_array(i, self._S_l()))
+            treatment.append(self._sum_array(i, self._S_l(), self.p))
 
         return treatment
 
@@ -68,81 +166,39 @@ class Simulator:
         control = [] 
 
         for i in np.arange(self.num_control):
-            control.append(self._sum_array(-(i+1), self.lam))
+            control.append(self._sum_array(-(i+1), self.lam, self.p))
 
         return control
 
-
-    def _g_e(self):
-        return self.sgRNAs[0: self.num_e]      
-
-    def _g_d(self):
-        return self.sgRNAs[self.num_e: self.num_e + self.num_d]
-
-    def _g_ntc(self):
-        return self.sgRNAs[self.num_e + self.num_d: self.num_e + self.num_d + self.num_ntc]
-
-    def _g_n(self):
-        return self.sgRNAs[self.num_e + self.num_d + self.num_ntc: self.num_e + self.num_d + self.num_ntc + self.num_n]
-
-    def _S(self):
-
-        # currently different each time called, the S_l in dataframe doens't reflect the one used for treatments,
-        # add in constructor?
-
-        S = []
-
-        g_e = self._g_e()
-        g_d = self._g_d()
-        g_ntc = self._g_ntc()
-        g_n = self._g_n()
-
-        for i in g_e:
-            g_scalar = np.random.uniform(1.2, 2.0)
-            for n in np.arange(i):
-                S.append(g_scalar)
-
-        for i in g_d:
-            g_scalar = np.random.uniform(0.2, 1.0)
-            for n in np.arange(i):
-                S.append(g_scalar)
-
-        for i in g_ntc:
-            for n in np.arange(i):
-                S.append(1)
-
-        for i in g_n:
-            for n in np.arange(i):
-                S.append(1)
-
-        return S 
-
     def _S_l(self):
-        return np.multiply(self._S(), self.lam)
-
-
+        return np.multiply(self.S, self.lam)
 
     def _type_of_change(self):
+        """
+        Sets number of enriched, depleted, NTC, and normal genes. 
+    @@ -169,13 +241,18 @@ def _type_of_change(self):
+            initialization.
+            
+        """
 
         type_of_change = []
 
-        g_e = self._g_e()
-        g_d = self._g_d()
-        g_ntc = self._g_ntc()
-        g_n = self._g_n()
-
-        e = ["enriched" for i in np.arange(len(g_e)) for n in np.arange(g_e[i])]
-        d = ["depleted" for i in np.arange(len(g_d)) for n in np.arange(g_d[i])]
-        ntc = ["ntc" for i in np.arange(len(g_ntc)) for n in np.arange(g_ntc[i])]
-        n = ["normal" for i in np.arange(len(g_n)) for n in np.arange(g_n[i])]
+        e = ["enriched" for i in np.arange(len(self.g_e)) for n in np.arange(self.g_e[i])]
+        d = ["depleted" for i in np.arange(len(self.g_d)) for n in np.arange(self.g_d[i])]
+        ntc = ["ntc" for i in np.arange(len(self.g_ntc)) for n in np.arange(self.g_ntc[i])]
+        n = ["normal" for i in np.arange(len(self.g_n)) for n in np.arange(self.g_n[i])]
 
         type_of_change = e + d + ntc + n
 
         return type_of_change 
 
-
     def sample(self):
-    
+        """
+        Generates DataFrame with observations for the simulation. 
+    @@ -188,12 +265,16 @@ def sample(self):
+            _setting_control_libraries(), _type_of_change() methods.  
+            
+        """
 
         # reorganize this to make code clearer
         sgRNA = pd.DataFrame({"sgRNAs": self._sgRNAs()})
