5bc1656cbfc996daa6997d0618958964adebada8
smouline <safaamouline@berkeley.edu>
2022-07-13 16:20:35 -0700

    fixed the version number

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="screenSim",
-    version="0.1.0",
+    version="0.0.1",
     author="Safaa Mouline",
     author_email="safaamouline@berkeley.edu",
     packages=["screenSim"],
-    description="pooled genetic screen simulator for CRISPR perturbations",
+    description="simulator for CRISPRi screens",
     install_requires=[
         'numpy', 
         'pandas',
