cf1052f91f1bbc3c60215598f990b923ea382a65
smouline <108255302+smouline@users.noreply.github.com>
2022-07-13 16:18:13 -0700

    Adjusting scalar parameter (#35)
    * added scalar mins and maxes for enrichment and depletion into the constructor, with defaults that are great differences (to test with pyscreen), added the scalars to the sample() dataframe
    
    * changed Simulator object initialization into a fixture

--- test/test_simulator.py
+++ test/test_simulator.py
@@ -1,45 +1,51 @@
 import pytest
 import numpy as np
+import pandas as pd
 from screenSim.simulator import Simulator
 
-# add more later  
+# add more later 
+# make it try many dataframes
 
-trial = Simulator(num_genes = 10)
+@pytest.fixture
+def df() -> pd.DataFrame:
+    return Simulator(num_genes = 10)
     
-def test_genes():
-    assert trial.num_genes == 10
+def test_genes(df):
+    assert df.num_genes == 10
 
-def test_fractions():
-    assert trial.fraction_enriched + trial.fraction_depleted + trial.fraction_NTC + trial.fraction_normal == 1
+def test_fractions(df):
+    assert df.fraction_enriched + df.fraction_depleted + df.fraction_NTC + df.fraction_normal == 1
      
-def test_totals_array():
-    for i in trial.totals_array:
-        assert (i < trial.max_total) & (i > trial.min_total)
+def test_totals_array(df):
+    for i in df.totals_array:
+        assert (i < df.max_total) & (i > df.min_total)
     
-def test_totals_array_2():
-    assert len(trial.totals_array) == trial.num_control + trial.num_treatment
+def test_totals_array_2(df):
+    assert len(df.totals_array) == df.num_control + df.num_treatment
     
-def test_num_types():
-    assert len(trial.g_e) + len(trial.g_d) + len(trial.g_ntc) + len(trial.g_n) == trial.num_genes
+def test_num_types(df):
+    assert len(df.g_e) + len(df.g_d) + len(df.g_ntc) + len(df.g_n) == df.num_genes
     
-def test_lambda(): 
-    for i in trial.lam:
-        assert (i > trial.bounds[0]) & (i < trial.bounds[1])
+def test_lambda(df): 
+    for i in df.lam:
+        assert (i > df.bounds[0]) & (i < df.bounds[1])
 
-def test_control_sums():
-    controls = trial._setting_control_libraries()
-    total = trial.totals_array
+def test_control_sums(df):
+    controls = df._setting_control_libraries()
+    total = df.totals_array
     for i in range(len(controls)):
         assert abs((controls[i].sum() - total[i])/total[i]) < 0.05
 
-def test_treatment_sums():
-    treatments = trial._setting_treatment_libraries()
-    total = trial.totals_array
+def test_treatment_sums(df):
+    treatments = df._setting_treatment_libraries()
+    total = df.totals_array
     for i in range(len(treatments)):
         assert abs((treatments[i].sum() - total[-(i+1)])/total[-(i+1)]) < 0.05
         
-def test_sgRNA_average():
-    assert (np.mean(trial.sgRNAs) - trial.avg_num_sgRNAs)/trial.avg_num_sgRNAs < 0.10
+def test_sgRNA_average(df):
+    assert (np.mean(df.sgRNAs) - df.avg_num_sgRNAs)/df.avg_num_sgRNAs < 0.10
+    
+
         
         
         
