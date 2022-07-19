97e28f14ba0dc18a401931904485f9386fd5f652
smouline <safaamouline@berkeley.edu>
2022-07-19 16:22:36 -0700

    more changes to speed initialization including getting rid of loops in _init_S and using np.repeat instead, creating a filled list in _modification() first, and only calculating the probabilities for negative binomial if the distribution is negative binomial. Cleaned up some documentation. Init time of last commit in this branch: about 7ms, now: about 2ms.

--- screenSim/simulator.py
+++ screenSim/simulator.py
@@ -28,12 +28,12 @@ class Simulator:
         ----------
         num_genes : int
             Number of genes.
-        avg_num_sgRNAs : int
-            Average number of sgRNAs across all genes. 
+        num_sgRNAs_per_gene : int
+            Number of sgRNAs per gene.  
         num_treatment : int
-            Number of treatment of libraries.
+            Number of treatment libraries.
         num_control : int
-            Number of control of libraries. 
+            Number of control libraries. 
         min_total : int
             The lower bound of the total number of counts for one library. 
         max_total : int
@@ -47,7 +47,7 @@ class Simulator:
         fraction_NTC : float
             The fraction of NTC genes with respect to all genes.
         type_dist : str
-            either "poisson" or "negative binomial" distribution. 
+            Either "poisson" or "negative binomial" distribution. 
         
         """ 
         
@@ -69,6 +69,7 @@ class Simulator:
         self._init_fractions(fraction_enriched, fraction_depleted, fraction_NTC)
         self._num_sgRNA()
         self._split_genes()
+        self._split_sgRNAs()
         self._init_sgRNA()
         self._init_gene()
         self._init_lambda()
@@ -79,7 +80,7 @@ class Simulator:
      
     def _init_count_totals(self):
         """
-        Initializes the totals of the sgRNA counts for each library. 
+        Initializes total sgRNA counts for each library. 
         
         """
         self.totals_array = np.random.randint(self.min_total, self.max_total, size = self.num_treatment + self.num_control)
@@ -106,57 +107,42 @@ class Simulator:
     
     def _num_sgRNA(self):
         """
-        Generates a number of sgRNAs per gene. 
-        
-        Returns
-        -------
-        sgRNAs : array
-            The values of the array follow a normal distribution with the
-            mean being `avg_num_sgRNAs`.
+        Calculates total number of sgRNAs.  
         
         """
         self.num_sgRNAs = self.num_genes * self.num_sgRNAs_per_gene
     
     def _split_genes(self):
         """
-        Splits genes into enriched, depleted, ntc, or normal based on the fractions.
+        Calculates number of enriched and depleted genes based on fractions. 
         
         """
-        # sgRNAs
-        self.num_e = round(self.num_sgRNAs * self.fraction_depleted)
-        self.num_d = round(self.num_sgRNAs * self.fraction_depleted)
-        self.num_ntc = round(self.num_sgRNAs * self.fraction_NTC)
-        self.num_n = round(self.num_sgRNAs * self.fraction_normal)
-        
-        # genes
         self.num_g_e = round(self.num_genes * self.fraction_depleted)
         self.num_g_d = round(self.num_genes * self.fraction_depleted)
-         
-    
-    def _init_sgRNA(self) -> list:
-        """
-        Generates list of numbered sgRNAs for use in sample() DataFrame.
         
-        Returns
-        ------
-        list 
-            All sgRNAs numbered.
+    def _split_sgRNAs(self):
+        """
+        Calculates number of enriched, depleted, and ntc sgRNAs based on fractions. 
+        
+        """
+        self.num_e = round(self.num_sgRNAs * self.fraction_depleted)
+        self.num_d = round(self.num_sgRNAs * self.fraction_depleted)
+        self.num_ntc = round(self.num_sgRNAs * self.fraction_NTC)     
+    
+    def _init_sgRNA(self):
+        """
+        Initializes array with sequential numbers representing sgRNA numbers. 
         
         """ 
         self.sgRNA = np.arange(self.num_sgRNAs)
     
     def _init_gene(self) -> list:
         """
-        Generates list of numbered genes for use in sample() DataFrame. 
-        
-        Returns
-        -------
-        list
-            All genes numbered. Genes repeated for each of their sgRNAs. 
+        Initializes array with numbers representing gene numbers. 
         
         """
         gene = np.arange(20000) 
-        self.gene = np.repeat(gene, 5)
+        self.gene = np.repeat(gene, self.num_sgRNAs_per_gene)
     
     def _init_lambda(self):
         """
@@ -167,10 +153,13 @@ class Simulator:
 
     def _init_p(self):
         """
-        Initializes a p (probability for negative binomial) for each sgRNA.
+        Initializes a probability for each sgRNA if the distribution is negative binomial.
         
         """
-        self.p = np.random.random(size = self.num_sgRNAs)
+        if self.type_dist == "negative binomial":
+            self.p = np.random.random(size = self.num_sgRNAs)
+        else:
+            self.p = 0
     
     def _init_S(self):
         """
@@ -180,54 +169,33 @@ class Simulator:
         S = np.ones(self.num_sgRNAs)
         
         gene_e_scalars = np.random.uniform(self.scalar_e_min, self.scalar_e_max, size = self.num_g_e)
-        
         gene_d_scalars = np.random.uniform(self.scalar_d_min, self.scalar_d_max, size = self.num_g_d)
         
-        mask = np.arange(self.num_sgRNAs).reshape(self.num_genes, self.num_sgRNAs_per_gene)
+        S[:self.num_e] = np.repeat(gene_e_scalars, self.num_sgRNAs_per_gene)
+        S[self.num_e: self.num_e + self.num_d] = np.repeat(gene_d_scalars, self.num_sgRNAs_per_gene)
         
-        shift = gene_e_scalars.size
-        
-        for idx, scalar in enumerate(gene_e_scalars):
-            S[mask[idx]] = scalar
-
-        for idx, scalar in enumerate(gene_d_scalars):
-            S[mask[idx + shift]] = scalar
-    
         self.S = S 
         
-    def _init_S_l(self) -> np.ndarray:
+    def _init_S_l(self):
         """
-        Scales the lambdas for treatment libraries. 
-        
-        Returns
-        -------
-        np.ndarray 
-            Element-wise product of `S` and `lam`.  
+        Scales the lambdas for treatment libraries by performing an element-wise product of `S` and `lam`.
             
         """
         self.S_l = np.multiply(self.S, self.lam)
      
     def _init_modification(self) -> list:
         """
-        Labels genes as enriched, depleted, NTC, or normal.
+        Labels each sgRNA as enriched, depleted, ntc, or normal. 
         
-        Returns
-        -------
-        type_of_change : list
-            Strings of enriched, depleted, NTC, and normal for each gene, 
-            based on the fractional representation specified upon 
-            initialization.
-            
         """
         
-        modification = []
+        mod = ["normal"] * self.num_sgRNAs
         
-        e = ["enriched"] * self.num_e
-        d = ["depleted"] * self.num_d
-        ntc = ["ntc"] * self.num_ntc
-        n = ["normal"] * self.num_n
+        mod[:self.num_e] = ["enriched"] * self.num_e
+        mod[self.num_e: self.num_e + self.num_d] = ["depleted"] * self.num_d
+        mod[self.num_e + self.num_d: self.num_e + self.num_d + self.num_ntc] = ["ntc"] * self.num_ntc
         
-        self.modification = e + d + ntc + n
+        self.modification = mod
     
     def _sum_array(self, index: int, lambdas: np.ndarray, p_array: np.ndarray) -> np.ndarray:
         """
@@ -259,7 +227,7 @@ class Simulator:
         elif self.type_dist == "negative binomial":
             a = np.random.negative_binomial(lambdas, p_array)
         else:
-            raise Exception("Make sure to choose a distribution from those available")
+            raise Exception("Make sure to choose a distribution from those available.")
         
         a = a.astype(float)
         a /= (a.sum())
@@ -268,33 +236,22 @@ class Simulator:
         
         return a
     
-    def _setting_control_libraries(self, control: pd.DataFrame) -> list:
+    def _setting_control_libraries(self, control: pd.DataFrame):
         """
-        Generates values for control libraries.
-        
-        Returns
-        -------
-        control : list
-            List of arrays, one for each library, generated by the _sum_array() method. 
+        Generates values for control libraries with _sum_array() and appends each library as a column to passed pd.DataFrame.
             
         """
         for i in np.arange(self.num_control):
             control[f"control_{i}"] = self._sum_array(i, self.lam, self.p)
     
-    def _setting_treatment_libraries(self, treatment: pd.DataFrame) -> list:
+    def _setting_treatment_libraries(self, treatment: pd.DataFrame):
         """
-        Generates values for treatment libraries.
-        
-        Returns
-        -------
-        treatment : list
-            List of arrays, one for each library, generated by the _sum_array() method. 
+        Generates values for treatment libraries with _sum_array() and appends each library as a column to passed pd.DataFrame.
             
         """
         for i in np.arange(self.num_treatment):
             treatment[f"treatment_{i}"] = self._sum_array(-(i+1), self.S_l, self.p)
     
-            
     def sample(self, seed: int = 10) -> pd.DataFrame:
         """
         Generates DataFrame with observations for the simulation. 
@@ -307,9 +264,8 @@ class Simulator:
         
         Returns
         -------
-        result : DataFrame 
-            sgRNA, gene, lam, S_lam, control, treatments, and modification DataFrames concatenated   
-            
+        result : pd.DataFrame 
+            sgRNA, gene, lambda, scalar, scaled lambda, modification, and each control and treatment library as columns
         """
         
         np.random.seed(seed)
