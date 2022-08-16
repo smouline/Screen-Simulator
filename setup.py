bb0524d801ab84e328ff7a72171924bdef633034
smouline <safaamouline@berkeley.edu>
2022-08-16 11:45:31 -0700

    added url to setup, setup.py now passes check

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name="screenSim",
     version="0.0.5",
+    description="simulator for count data of CRISPR screens",
+    url = "https://github.com/smouline/Screen-Simulator",
     author="Safaa Mouline",
     author_email="safaamouline@berkeley.edu",
     packages=["screenSim"],
-    description="simulator for CRISPRi screens",
     install_requires=[
         'numpy', 
         'pandas',
